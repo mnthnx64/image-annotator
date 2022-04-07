9ee40128d38ece81e049ae33e5c9be040e6acce2
Hemant Sambhaji Kolte <69529482+hemant-kolte@users.noreply.github.com>
2022-04-07 13:42:27 -0700

    Update separatingimages.py

--- Separating Image code/separatingimages.py
+++ Separating Image code/separatingimages.py
@@ -1,18 +1,12 @@
-# import shutil
-# import os
-
-# os.chdir('C:\Users\hkolte\Documents\Sem 2\Perception\datasets\Code\test subject\MVI_39051')
-# dst_dir = "C:\Users\hkolte\Documents\Sem 2\Perception\datasets\Code\test subject\Dest"
-# for f in os.listdir():
-#     shutil.copy(f, dst_dir)
 
 import glob
 import shutil
 import os
 import re
 
-src_dir = ".\\xyz"
-dst_dir = ".\Dest"
+src_dir = ".\\Insight-MVT_Annotation_Test"
+os.mkdir(".\Sorted_Images")
+dst_dir = ".\Sorted_Images"
 list = glob.iglob(os.path.join(src_dir,"*","*.jpg"))
 pattern = re.compile(r"[a-zA-Z]+(\d\d\d\d\d)+")
 count = 0
